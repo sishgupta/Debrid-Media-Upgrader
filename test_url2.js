0c2e9f8bd324465ca77e531ff19f6b89616f6df4
sishgupta <sishgupta@gmail.com>
2026-05-05 13:24:56 +0000

    refactor: Remove unused test files
    The following test files have been removed as they are no longer necessary:
    - test_streams.js
    - test_unroll.js
    - test_url.js
    - test_url2.js
    - test_url3.js

--- test_url2.js
+++
@@ -1,5 +0,0 @@
-import fetch from 'node-fetch'; // if available, or just global fetch
-
-const url = "https://aiostreamsfortheweebs.midnightignite.me/YOUR_CONFIG_STRING/stream/movie/tt0497116.json";
-
-// We need to fetch from the actual user config string!
